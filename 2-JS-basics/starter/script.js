34fa5cc4bd272fe6d0fd1ca5856c55e3e32388c3
enzo2605 <34918941+enzo2605@users.noreply.github.com>
2020-03-02 18:35:56 +0100

    End section 2: Js basics

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -194,6 +194,26 @@ if (BMIMark > BMIJohn) {
 
 
 /*****************************
+* Boolean logic
+*/
+/*
+var firstName = 'John';
+var age = 20;
+
+if (age < 13) {
+    console.log(firstName + ' is a boy.');
+} else if (age >= 13 && age < 20) {
+    console.log(firstName + ' is a teenager.');
+} else if (age >= 20 && age < 30) {
+    console.log(firstName + ' is a young man.');
+} else {
+    console.log(firstName + ' is a man.');
+}
+*/
+
+
+
+/*****************************
 * The Ternary Operator and Switch Statements
 */
 /*
@@ -250,9 +270,9 @@ switch (true) {
 /*****************************
 * Truthy and Falsy values and equality operators
 */
+/*
 // falsy values: undefined, null, 0, '', NaN
 // truthy values: NOT falsy values
-/*
 
 var height;
 
@@ -264,8 +284,87 @@ if (height || height === 0) {
     console.log('Variable has NOT been defined');
 }
 
-77 === '77' ? console.log("equals") : console.log("NOT equals");
+// Equality operators
+if (height === '23') {
+    console.log('The == operator does type coercion!');
+}
+*/
 
+
+
+/*****************************
+* CODING CHALLENGE 2
+*/
+
+/*
+John and Mike both play basketball in different teams. In the latest 3 games, John's team scored 89, 120 and 103 points, while Mike's team scored 116, 94 and 123 points.
+
+1. Calculate the average score for each team
+2. Decide which teams wins in average (highest average score), and print the winner to the console. Also include the average score in the output.
+3. Then change the scores to show different winners. Don't forget to take into account there might be a draw (the same average score)
+
+4. EXTRA: Mary also plays basketball, and her team scored 97, 134 and 105 points. Like before, log the average winner to the console. HINT: you will need the && operator to take the decision. If you can't solve this one, just watch the solution, it's no problem :)
+5. Like before, change the scores to generate different winners, keeping in mind there might be draws.
+
+GOOD LUCK 😀
+*/
+/*
+var scoreJohn = (189 + 120 + 103) / 3;
+var scoreMike = (129 + 94 + 123) / 3;
+var scoreMary = (97 + 134 + 105) / 3;
+console.log(scoreJohn, scoreMike, scoreMary);
+
+if (scoreJohn > scoreMike && scoreJohn > scoreMary) {
+    console.log('John\'s team wins with ' + scoreJohn + ' points');
+} else if (scoreMike > scoreJohn && scoreMike > scoreMary) {
+    console.log('Mike\'s team wins with ' + scoreMike + ' points');
+} else if (scoreMary > scoreJohn && scoreMary > scoreMike) {
+    console.log('Mary\'s team wins with ' + scoreMary + ' points');
+} else {
+    console.log('There is a draw');
+}
+
+
+if (scoreJohn > scoreMike) {
+    console.log('John\'s team wins with ' + scoreJohn + ' points');
+} else if (scoreMike > scoreJohn) {
+    console.log('Mike\'s team wins with ' + scoreMike + ' points');
+} else {
+    console.log('There is a draw');
+}
+*/
+
+
+
+/*****************************
+* Functions
+*/
+/*
+function calculateAge(birthYear) {
+    return 2018 - birthYear;
+}
+
+var ageJohn = calculateAge(1990);
+var ageMike = calculateAge(1948);
+var ageJane = calculateAge(1969);
+console.log(ageJohn, ageMike, ageJane);
+
+
+function yearsUntilRetirement(year, firstName) {
+    var age = calculateAge(year);
+    var retirement = 65 - age;
+    
+    if (retirement > 0) {
+        console.log(firstName + ' retires in ' + retirement + ' years.');
+    } else {
+        console.log(firstName + ' is already retired.')
+    }
+    
+}
+
+yearsUntilRetirement(1990, 'John');
+yearsUntilRetirement(1948, 'Mike');
+yearsUntilRetirement(1969, 'Jane');
 */
 
 
@@ -274,27 +373,26 @@ if (height || height === 0) {
 * Function Statements and Expressions
 */
 /*
-function whatDoYouDo(job, firstName) {
-    
-}
-/*
+// Function declaration
+// function whatDoYouDo(job, firstName) {}
+
+// Function expression
 var whatDoYouDo = function(job, firstName) {
-    switch (job) {
+    switch(job) {
         case 'teacher':
             return firstName + ' teaches kids how to code';
-
         case 'driver':
-            return firstName + ' drives a cab in Lisbon';
-
+            return firstName + ' drives a cab in Lisbon.'
         case 'designer':
             return firstName + ' designs beautiful websites';
-
         default:
             return firstName + ' does something else';
     }
 }
 
 console.log(whatDoYouDo('teacher', 'John'));
+console.log(whatDoYouDo('designer', 'Jane'));
+console.log(whatDoYouDo('retired', 'Mark'));
 */
 
 
@@ -307,18 +405,16 @@ console.log(whatDoYouDo('teacher', 'John'));
 var names = ['John', 'Mark', 'Jane'];
 var years = new Array(1990, 1969, 1948);
 
-console.log(names);
+console.log(names[2]);
+console.log(names.length);
 
-//Mutate array data
+// Mutate array data
 names[1] = 'Ben';
-console.log(names);
-
 names[names.length] = 'Mary';
 console.log(names);
-*/
-/*
-//Different data types
-var john = ['John', 'Smith', 1990, 'teacher', false];
+
+// Different data types
+var john = ['John', 'Smith', 1990, 'designer', false];
 
 john.push('blue');
 john.unshift('Mr.');
@@ -331,9 +427,52 @@ console.log(john);
 
 console.log(john.indexOf(23));
 
-var isDesigner = john.indexOf('designer') === -1 ? 'John is NOT a designer' : 'John is a designer';
+var isDesigner = john.indexOf('designer') === -1 ? 'John is NOT a designer' : 'John IS a designer';
 console.log(isDesigner);
+*/
 
+
+
+/*****************************
+* CODING CHALLENGE 3
+*/
+
+/*
+John and his family went on a holiday and went to 3 different restaurants. The bills were $124, $48 and $268.
+
+To tip the waiter a fair amount, John created a simple tip calculator (as a function). He likes to tip 20% of the bill when the bill is less than $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
+
+In the end, John would like to have 2 arrays:
+1) Containing all three tips (one for each bill)
+2) Containing all three final paid amounts (bill + tip).
+
+(NOTE: To calculate 20% of a value, simply multiply it with 20/100 = 0.2)
+
+GOOD LUCK 😀
+*/
+/*
+function tipCalculator(bill) {
+    var percentage;
+    if (bill < 50) {
+        percentage = .2;
+    } else if (bill >= 50 && bill < 200) {
+        percentage = .15;
+    } else {
+        percentage = .1;
+    }
+    return percentage * bill;
+}
+
+var bills = [124, 48, 268];
+var tips = [tipCalculator(bills[0]),
+            tipCalculator(bills[1]),
+            tipCalculator(bills[2])];
+
+var finalValues = [bills[0] + tips[0],
+                   bills[1] + tips[1],
+                   bills[2] + tips[2]];
+
+console.log(tips, finalValues);
 */
 
 
@@ -341,6 +480,7 @@ console.log(isDesigner);
 /*****************************
 * Objects and properties
 */
+/*
 // Object literal
 var john = {
     firstName: 'John',
@@ -360,8 +500,69 @@ john.job = 'designer';
 john['isMarried'] = true;
 console.log(john);
 
+// new Object syntax
 var jane = new Object();
 jane.firstName = 'Jane';
 jane.birthYear = 1969;
 jane['lastName'] = 'Smith';
 console.log(jane);
+*/
+
+
+
+/*****************************
+* Objects and methods
+*/
+/*
+var john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    birthYear: 1992,
+    family: ['Jane', 'Mark', 'Bob', 'Emily'],
+    job: 'teacher',
+    isMarried: false,
+    calcAge: function() {
+        this.age = 2018 - this.birthYear;
+    }
+};
+
+john.calcAge();
+console.log(john);
+*/
+
+
+
+/*****************************
+* CODING CHALLENGE 4
+*/
+
+/*
+Let's remember the first coding challenge where Mark and John compared their BMIs. Let's now implement the same functionality with objects and methods.
+1. For each of them, create an object with properties for their full name, mass, and height
+2. Then, add a method to each object to calculate the BMI. Save the BMI to the object and also return it from the method.
+3. In the end, log to the console who has the highest BMI, together with the full name and the respective BMI. Don't forget they might have the same BMI.
+
+Remember: BMI = mass / height^2 = mass / (height * height). (mass in kg and height in meter).
+
+GOOD LUCK 😀
+*/
+//Notazione a costruttore. 'this' è riferito all'oggetto.
+function person (fullName, mass, height) {
+    this.fullName = fullName;
+    this.mass = mass;
+    this.height = height;
+    this.calculateBMI = function () {
+        return this.mass / (this.height * this.height);
+    };
+}
+
+let John, Mark, johnBMI, markBMI; 
+
+//creazione dei due nuovi oggetti.
+John = new person('John Smith', 76, 1.7);
+Mark = new person('Mark Hammer', 89, 1.8);
+
+johnBMI = John.calculateBMI();
+markBMI = Mark.calculateBMI();
+
+(johnBMI > markBMI) ? console.log(John.fullName + ' ' + johnBMI) : console.log(Mark.fullName + ' ' + markBMI);
