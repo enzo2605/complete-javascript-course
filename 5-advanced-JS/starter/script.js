21b1ea820e45e6347579c0fc56d25985be4c563c
enzo2605 <34918941+enzo2605@users.noreply.github.com>
2020-03-13 13:23:15 +0100

    Try to complete the coding challenge

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -175,7 +175,7 @@ function game() {
     console.log(score >= 5);
 }
 game();
-*/
+
 (function() {
     var score = Math.random() * 10;
     console.log(score >= 5);
@@ -186,4 +186,188 @@ game();
 (function(goodLuck) {
     var score = Math.random() * 10;
     console.log(score >= 5 - goodLuck);
-})(5);
+})(5);*/
+
+/////////////////////////////
+// Lecture: Closures
+
+/*
+function retirement(retirementAge) {
+    var a = ' years left until retirement.';
+    return function(yearOfBirth) {
+        var age = 2016 - yearOfBirth;
+        console.log((retirementAge - age) + a);
+    }
+}
+
+var retirementUS = retirement(66);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+retirementUS(1990);
+retirementGermany(1990);
+retirementIceland(1990);
+
+function interviewQuestion(job) {
+    return function (name) {
+        if (job === 'designer') {
+            console.log(name + ', can you please explain what UX design is?');
+        } else if (job === 'teacher') {
+            console.log('What subject do you teach, ' + name + '?');
+        } else {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
+
+interviewQuestion('designer')('Vincenzo');
+
+//retirement(66)(1990);
+
+*/
+
+
+
+/////////////////////////////
+// Lecture: Bind, call and apply
+/*
+var john = {
+    name: 'John',
+    age: 26,
+    job: 'teacher',
+    presentation: function (style, timeOfDay) {
+        if(style === 'formal') {
+            console.log('Good ' + timeOfDay + ' ladies and gentlemen! I\'m ' + this.name + ', I\'m ' + this.job + ', I\'m ' + this.age + ' years old.');
+        } else if(style ===  'friendly') {
+            console.log('Hey! What\'up? I\'m' + this.name + ', I\'m' + this.job + ' I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay);
+        }
+    }
+};
+
+var emily = {
+    name: 'Emily',
+    age: 35,
+    job: 'designer'
+};
+
+john.presentation('formal', 'morning');
+
+john.presentation.call(emily, 'friendly', 'afternoon');
+
+var johnFriendly = john.presentation.bind(john, 'friendly');
+
+johnFriendly('morning');
+johnFriendly('night');
+
+// Another cool example
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el) {
+    return 2016 - el;
+}
+
+function isFullAge(limit, el) {
+    return el >= limit;
+}
+
+var a = arrayCalc(years, calculateAge);
+console.log(a);
+var japan = arrayCalc(a, isFullAge.bind(this, 20));
+console.log(japan);
+*/
+
+/////////////////////////////
+// CODING CHALLENGE
+
+
+/*
+--- Let's build a fun quiz game in the console! ---
+
+1. Build a function constructor called Question to describe a question. A question should include:
+    a) question itself
+    b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
+    c) correct answer (I would use a number for this)
+
+2. Create a couple of questions using the constructor
+
+3. Store them all inside an array
+
+4. Select one random question and log it on the console, together with the possible answers (each question should have a number) 
+   (Hint: write a method for the Question objects for this task).
+
+5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
+
+6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
+
+7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the 
+   other programmers code (Hint: we learned a special technique to do exactly that).
+*/
+
+(function () {
+    //1.
+    function Question(question, answers, correct) {
+        this.question = question;
+        this.answers = answers;
+        this.correct = correct;
+    }
+
+    //4.
+    Question.prototype.displayQuestion = function() {
+        
+        console.log(this.question);
+
+        for (var i = 0; i < this.answers.length; i++) {
+            console.log(i + ': ' + this.answers[i]);
+        }
+    }
+
+    //6.
+    Question.prototype.checkAnswer = function (ans) {
+        if(ans === this.correct)
+            console.log('Correct!');
+        else
+            console.log('Wrong');
+    }
+
+    //2.
+    var q1 = new Question('Is Naples in Italy?', ['Yes', 'No'], 0);
+    var q2 = new Question('What\'s my name?', ['Luca', 'Giovanni', 'Vincenzo'], 2);
+    var q3 = new Question('How old am I?', ['22', '30', '21'], 2);
+
+    //3.
+    var questions = [q1, q2, q3];
+
+    //4.
+    var n = Math.floor(Math.random() * questions.length);
+    questions[n].displayQuestion();
+
+    //5.
+    //var ans = parseInt(prompt('Select the correct answer'));
+
+    //6.
+    questions[n].checkAnswer(ans);
+})();
+
+/*
+--- Expert level ---
+
+8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right 
+   after displaying the result)
+
+9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. 
+   In this case, DON'T call the function from task 8.
+
+10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power 
+   of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+
+11. Display the score in the console. Use yet another method for this.
+
+*/
